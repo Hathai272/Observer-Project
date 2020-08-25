9ba7c407592b41b04bcc466752ed67086adc4648
Hathai <hathaithk@gmail.com>
2020-08-25 13:30:21 +0700

    import observer&observable

--- observer/src/observer/Marketing.java
+++ observer/src/observer/Marketing.java
@@ -4,13 +4,16 @@
  */
 package observer;
 
+import java.util.Observer;
+import java.util.Observable;
+
 /**
  *
  * @author sarun
  */
-public class Marketing implements MyObserver {
+public class Marketing implements Observer {
     @Override
-    public void update(Source o) {
+    public void update(Observable o, Object arg) {
         System.out.println(
 	 "(marketing) headquater is updated to " 
                 + ((HeadQuater)o).getSomeData());
